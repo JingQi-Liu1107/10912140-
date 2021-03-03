2b53331cdea82f36d1280e35f15398f42da5978d
JingQi-Liu1107 <79981023+JingQi-Liu1107@users.noreply.github.com>
2021-03-03 21:20:00 +0800

    Add files via upload

---
+++ 12345.py
@@ -0,0 +1,10 @@
+from myMath import myArithmetic,myCalcArea,myStatistics
+def jimmy():
+  return a+b+c+d+e
+a=int(input('first'))
+b=int(input('second'))
+c=int(input('third'))
+d=int(input('fourth'))
+e=int(input('fifth'))
+ans=jimmy()
+print(myArithmetic.myDiv(ans, 5))
